c5366620624a13a9cf850c7128986e63e5f7a2a5
1chary <abhilashchary04@gmail.com>
2024-07-16 00:07:57 +0530

    Added Register and log in api

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const jwtToken = require("jsonwebtoken")
 
 const { open } = require("sqlite")
 const sqlite3 = require("sqlite3")
+const bcrypt = require("bcrypt")
 
 const path = require("path")
 const dbPath = path.join(__dirname,"mydata.db")
@@ -12,7 +13,7 @@ let db = null;
 
 
 
-const initialTheServer = async () => {
+const initializeTheServer = async () => {
     try {
         db = await open({
             filename: dbPath,
@@ -28,5 +29,48 @@ const initialTheServer = async () => {
     } 
 }
 
+// REGISTER NEW USER 
+// Add the user details to the request body using curl or insomania 
+app.post('/users', async (request, response) => {
+    const { username,email,password,role } = request.body;
+    const hashedPassword = await bcrypt.hash(request.body.password,10);
+    await db.run('INSERT INTO users (username,email,hashedPassword,role) VALUES (?, ?,?,?)', [username,email,password,role], (err) => {
+      if (err) {
+        return response.status(400).json({ error: 'Error adding user' });
+      }
+      response.status(201).json({ message: 'User created' });
+    });
+  });
 
-initialTheServer()
+
+  // Login user 
+  app.post("/login", async (request, response) => {
+    const { email, password } = request.body;
+    const getUserEmail = `
+      SELECT *
+      FROM users
+      where email = '${email}'
+      `;
+    const dbUser = await db.get(getUserEmail);
+    // It will check the user is available in data base or not if not displays invalid user
+    if (dbUser === undefined) { 
+      response.status(400);
+      response.send("Invalid User");
+    } 
+    // if available compares password with hashed password if wrong password given invalid password
+    else {
+      const checkPassword = await bcrypt.compare(password, dbUser.password);
+      if (checkPassword === true) {
+        response.send("Login Success");
+      } else {
+        response.status(400);
+        response.send("Invalid Password");
+      }
+    }
+  });
+  
+  
+
+
+
+initializeTheServer()
